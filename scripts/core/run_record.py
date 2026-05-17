78725b28871afe021900d1e0eb59f88ef7948a82
zjli2013 <leezhengjiang@126.com>
2026-05-17 17:01:25 +0800

    fix cam bug fps 5->6

--- scripts/core/run_record.py
+++ scripts/core/run_record.py
@@ -309,8 +309,6 @@ def run_record(record_cfg: RecordConfig):
         preprocessor = None
         postprocessor = None
 
-        robot.connect()
-
         # configure the teleop and policy
         if record_cfg.run_mode == "run_record":
             logging.info("====== [INFO] Running in teleoperation mode ======")
@@ -329,13 +327,14 @@ def run_record(record_cfg: RecordConfig):
             preprocessor, postprocessor = make_pre_post_processors(
                 policy_cfg=record_cfg.policy,
                 pretrained_path=record_cfg.policy.pretrained_path,
-                dataset_stats=rename_stats(dataset.meta.stats, {}),
+                dataset_stats=rename_stats(dataset.meta.stats, {}),  # 使用空字典作为rename_map
                 preprocessor_overrides={
                     "device_processor": {"device": record_cfg.policy.device},
-                    "rename_observations_processor": {"rename_map": {}},
+                    "rename_observations_processor": {"rename_map": {}},  # 使用空字典作为rename_map
                 },
             )
 
+        robot.connect()
         if teleop is not None:
             teleop.connect()
 
